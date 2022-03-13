2251d518677ce66f22e11de82bb7522e3d5c3f4a
Iris <irisquerol1998@gmail.com>
2022-03-13 10:40:46 +0100

    Form create task get text done

--- app/src/main/java/com/android/e4todolist/MainActivity.java
+++ app/src/main/java/com/android/e4todolist/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,11 +16,14 @@ import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton fab;
-    FragmentManager fm = getSupportFragmentManager();
-    Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+    //FragmentManager fm = getSupportFragmentManager();
+    //Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        checkIntent();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().findFragmentById(R.id.list);
@@ -27,12 +32,27 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (fragment == null) {
-                    fragment = new CreateTaskFragment();
-                    fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-                }
+                openCreateTaskActivity();
 
             }
         });
+    }
+
+    public void openCreateTaskActivity() {
+        Intent intent = new Intent(MainActivity.this, CreateActivityTask.class);
+        // intent.putExtra("subjectID", );
+        startActivity(intent);
+    }
+
+    public void addItemToList() {
+
+    }
+
+    public void checkIntent() {
+        if (getIntent().getExtras() != null) {
+            Intent intentGetter = getIntent();
+            String strValue = intentGetter.getStringExtra("taskTitle");
+            Log.d("myTag", "This is the  variable value: " + strValue);
+        }
     }
 }
