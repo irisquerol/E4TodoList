8ccb905b21a413c3abcb54da5814cc2bc69ddcac
martifarre <martifarre99@gmail.com>
2022-03-20 00:30:57 +0100

    taskmanager javadoc

--- app/src/main/java/com/android/e4todolist/Model/TaskManager.java
+++ app/src/main/java/com/android/e4todolist/Model/TaskManager.java
@@ -15,6 +15,10 @@ public class TaskManager {
     ArrayList<Task> taskList;
     Context context;
 
+    /**
+     * Creates the default tasks and the class singelton, if there are values stored it loads.
+     * @param context  context
+     */
     private TaskManager(Context context) {
         this.taskList = new ArrayList<>();
         this.context = context;
@@ -33,15 +37,23 @@ public class TaskManager {
         }
     }
 
+    /**
+     * Gets instance of singelton
+     */
     public static TaskManager getInstance(Context context) {
         if (single_instance == null) single_instance = new TaskManager(context);
         return single_instance;
     }
-
+    /**
+     * Gets the instance of singelton
+     */
     public static TaskManager getInstance() {
         return single_instance;
     }
 
+    /**
+     * Saves the tasks in the phone memory
+     */
     public void saveTasks() {
         Gson gson = new Gson();
         String json = gson.toJson(taskList);
@@ -53,14 +65,23 @@ public class TaskManager {
         editor.apply();
     }
 
+    /**returns tasks list
+     * @return list task
+     */
     public ArrayList<Task> getTaskList() {
         return taskList;
     }
 
+    /**Adds a task
+     * @param task task
+     */
     public void addTask(Task task) {
         taskList.add(task);
     }
 
+    /**Removes a task
+     * @param idTask int
+     */
     public void removeTask(int idTask) {
         taskList.remove(idTask);
     }
